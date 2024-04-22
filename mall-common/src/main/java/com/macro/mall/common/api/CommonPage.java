3d83b265e9dbddcc0e42cdfe7db957200b3166bf
MXiangHAO <1953686710@qq.com>
2024-04-22 17:23:49 +0800

    --task=1023079 --user=龚仕元 分页数据封装类 https://www.tapd.cn/40489619/s/1086274
    --task=1023079 --user=龚仕元 分页数据封装类 https://www.tapd.cn/40489619/s/1086274

--- mall-common/src/main/java/com/macro/mall/common/api/CommonPage.java
+++ mall-common/src/main/java/com/macro/mall/common/api/CommonPage.java
@@ -1,14 +1,52 @@
 package com.macro.mall.common.api;
 
 import com.github.pagehelper.PageInfo;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 /**
  * 分页数据封装类
- * Created by macro on 2019/4/19.
+ * Created by mxh on 2024/4/22.
  */
 public class CommonPage<T> {
+    @Setter @Getter
+    private Integer pageNum;
+    @Setter @Getter
+    private Integer pageSize;
+    @Setter @Getter
+    private Integer totalPage;
+    @Setter @Getter
+    private Long total;
+    @Setter @Getter
+    private List<T> list;
+    /**
+     * 将PageHelper分页后的list转为分页信息
+     */
+    public static <T> CommonPage<T> restPage(List<T> list) {
+        CommonPage<T> result = new CommonPage<T>();
+        PageInfo<T> pageInfo = new PageInfo<T>(list);
+        result.setTotalPage(pageInfo.getPages());
+        result.setPageNum(pageInfo.getPageNum());
+        result.setPageSize(pageInfo.getPageSize());
+        result.setTotal(pageInfo.getTotal());
+        result.setList(pageInfo.getList());
+        return result;
+    }
+
+    /**
+     * 将SpringData分页后的list转为分页信息
+     */
+    public static <T> CommonPage<T> restPage(Page<T> pageInfo) {
+        CommonPage<T> result = new CommonPage<T>();
+        result.setTotalPage(pageInfo.getTotalPages());
+        result.setPageNum(pageInfo.getNumber());
+        result.setPageSize(pageInfo.getSize());
+        result.setTotal(pageInfo.getTotalElements());
+        result.setList(pageInfo.getContent());
+        return result;
+    }
 
 }
