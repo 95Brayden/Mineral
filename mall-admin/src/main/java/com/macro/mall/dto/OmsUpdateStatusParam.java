3015c9d6cab8b37e5b634cf9f7d11b122b6f6e4c
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-04-23 16:18:25 +0800

    订单退货申请和退货原因管理
    --task=1023212 --user=钱旭 订单退货申请和退货原因管理 https://www.tapd.cn/40489619/s/1088610

--- mall-admin/src/main/java/com/macro/mall/dto/OmsUpdateStatusParam.java
+++ mall-admin/src/main/java/com/macro/mall/dto/OmsUpdateStatusParam.java
@@ -8,9 +8,26 @@ import java.math.BigDecimal;
 
 /**
  * 确认收货提交参数
- * Created by macro on 2018/10/18.
+ * Created by qx on 2024/4/23.
  */
-
+@Getter
+@Setter
 public class OmsUpdateStatusParam {
+    @ApiModelProperty("服务单号")
+    private Long id;
+    @ApiModelProperty("收货地址关联id")
+    private Long companyAddressId;
+    @ApiModelProperty("确认退款金额")
+    private BigDecimal returnAmount;
+    @ApiModelProperty("处理备注")
+    private String handleNote;
+    @ApiModelProperty("处理人")
+    private String handleMan;
+    @ApiModelProperty("收货备注")
+    private String receiveNote;
+    @ApiModelProperty("收货人")
+    private String receiveMan;
+    @ApiModelProperty("申请状态：1->退货中；2->已完成；3->已拒绝")
+    private Integer status;
 
 }
