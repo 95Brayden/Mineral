1107d93e946ad80216465a3e331369b7931c7db5
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-04-23 15:00:57 +0800

    --task=1023019 --user=钱旭 swagger配置 https://www.tapd.cn/40489619/s/1084437

--- mall-admin/src/main/java/com/macro/mall/component/ResourceRoleRulesHolder.java
+++ mall-admin/src/main/java/com/macro/mall/component/ResourceRoleRulesHolder.java
@@ -10,7 +10,13 @@ import javax.annotation.PostConstruct;
  * 资源与角色访问对应关系操作组件
  * Created by macro on 2020/7/17.
  */
-
+@Component
 public class ResourceRoleRulesHolder {
+    @Autowired
+    private UmsResourceService resourceService;
 
+    @PostConstruct
+    public void initResourceRolesMap(){
+        resourceService.initResourceRolesMap();
+    }
 }
