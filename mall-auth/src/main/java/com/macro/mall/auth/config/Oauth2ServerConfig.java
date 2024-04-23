a144cdc164412e61b3fc661c40d804e73a7beaaa
MXiangHAO <1953686710@qq.com>
2024-04-23 16:53:35 +0800

    auth-config
    --task=1023123 --user=龚仕元 认证配置 https://www.tapd.cn/40489619/s/1089259

--- mall-auth/src/main/java/com/macro/mall/auth/config/Oauth2ServerConfig.java
+++ mall-auth/src/main/java/com/macro/mall/auth/config/Oauth2ServerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -24,11 +25,72 @@ import java.util.List;
 
 /**
  * 认证服务器配置
- * Created by macro on 2020/6/19.
+ * @author mxh
+ * @date 2024/4/23
  */
 @AllArgsConstructor
 @Configuration
+/*
+Spring Boot 应用中使用 OAuth2 来提供身份验证和授权服务
+授权服务自动配置: 开启授权服务器相关的自动配置，包括客户端配置、令牌存储、用户信息服务等。
+ 生成授权服务相关的端点: 自动创建一些 OAuth2 授权服务的端点，如 /oauth/token, /oauth/authorize 等。
+安全配置: 自动配置 OAuth2 相关的安全过滤器链，处理身份验证、令牌生成、令牌校验等。
+ */
 @EnableAuthorizationServer
-public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
 
+public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+    private final UserServiceImpl userDetailsService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenEnhancer jwtTokenEnhancer;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("admin-app")
+                .secret(passwordEncoder.encode("123456"))
+                .scopes("all")
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(3600*24)
+                .refreshTokenValiditySeconds(3600*24*7)
+                .and()
+                .withClient("portal-app")
+                .secret(passwordEncoder.encode("123456"))
+                .scopes("all")
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(3600*24)
+                .refreshTokenValiditySeconds(3600*24*7);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+        List<TokenEnhancer> delegates = new ArrayList<>();
+        delegates.add(jwtTokenEnhancer);
+        delegates.add(accessTokenConverter());
+        enhancerChain.setTokenEnhancers(delegates); //配置JWT的内容增强器
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService((UserDetailsService) userDetailsService) //配置加载用户信息的服务
+                .accessTokenConverter(accessTokenConverter())
+                .tokenEnhancer(enhancerChain);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients();
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setKeyPair(keyPair());
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public KeyPair keyPair() {
+        //从classpath下的证书中获取秘钥对
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), "123456".toCharArray());
+        return keyStoreKeyFactory.getKeyPair("jwt", "123456".toCharArray());
+    }
 }
--- mall-auth/src/main/java/com/macro/mall/auth/config/WebSecurityConfig.java
+++ mall-auth/src/main/java/com/macro/mall/auth/config/WebSecurityConfig.java
@@ -10,12 +10,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * SpringSecurity相关配置
- * Created by macro on 2020/6/19.
+ * Created by mxh on 2024/4/23.
  */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig{
 
+    /**
+     *它用于启用 Web 安全性功能
+     * 启用 Spring Security: 启用 Spring Security 的所有功能，包括身份验证、授权、攻击防护等。
+     * 自定义安全配置: 允许你在配置类中自定义安全规则、用户认证、授权等行为。
+     * 提供默认的安全配置: 如果你不提供任何自定义的安全配置，Spring Security 会提供一组默认的安全规则和行为。
+     */
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
@@ -25,5 +31,6 @@ public class WebSecurityConfig{
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 
 }
