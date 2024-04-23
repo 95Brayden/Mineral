ff219ca4da3aa76c5a1f80c6bbf98954bf250c5c
MXiangHAO <1953686710@qq.com>
2024-04-23 16:11:44 +0800

    create auth-domin
    --task=1023122 --user=龚仕元 用户和用户登录认证DAO https://www.tapd.cn/40489619/s/1088603

--- mall-auth/src/main/java/com/macro/mall/auth/domain/SecurityUser.java
+++ mall-auth/src/main/java/com/macro/mall/auth/domain/SecurityUser.java
@@ -13,7 +13,85 @@ import java.util.Collection;
  * 登录用户信息
  * Created by mxh on 2024/4/22.
  */
+@Data
+public class SecurityUser  implements  UserDetails{
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 用户密码
+     */
+    private String password;
+    /**
+     * 用户状态
+     */
+    private Boolean enabled;
+    /**
+     * 登录客户端ID
+     */
+    private String clientId;
+    /**
+     * 权限数据
+     * 用户可以有多种角色 比如会员 管理者 都将对于相应的权限
+     */
+    private Collection<SimpleGrantedAuthority> authorities;
 
-public class SecurityUser  {
+    public SecurityUser() {
+
+    }
+    public SecurityUser(UserDto userDto) {
+        this.setId(userDto.getId());
+        this.setUsername(userDto.getUsername());
+        this.setPassword(userDto.getPassword());
+        this.setEnabled(userDto.getStatus() == 1);
+        this.setClientId(userDto.getClientId());
+        //user的用户权限依次加入 权限集合
+        if (userDto.getRoles() != null) {
+            authorities = new ArrayList<>();
+            userDto.getRoles().forEach(item -> authorities.add(new SimpleGrantedAuthority(item)));
+        }
+    }
+
+    /**
+     * <?>或<? extends Something>是通配符泛型
+     * ?表示这个Collection可以持有任何类型的对象
+     * 只要这些对象是GrantedAuthority或其子类的实例。
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
 
 }
