912fc33d2e01a861a1ed0be456ae91f41d88c43b
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-04-23 23:55:19 +0800

    sku商品库存管理
    --task=1023238 --user=钱旭 sku商品库存管理 https://www.tapd.cn/40489619/s/1089748

--- mall-admin/src/main/java/com/macro/mall/controller/PmsSkuStockController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/PmsSkuStockController.java
@@ -13,9 +13,32 @@ import java.util.List;
 
 /**
  * sku库存Controller
- * Created by macro on 2018/4/27.
+ * Created by qx on 2024/4/23.
  */
 
+@Controller
+@Api(tags = "PmsSkuStockController", description = "sku商品库存管理")
+@RequestMapping("/sku")
 public class PmsSkuStockController {
+    @Autowired
+    private PmsSkuStockService skuStockService;
 
+    @ApiOperation("根据商品编号及编号模糊搜索sku库存")
+    @RequestMapping(value = "/{pid}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<PmsSkuStock>> getList(@PathVariable Long pid, @RequestParam(value = "keyword",required = false) String keyword) {
+        List<PmsSkuStock> skuStockList = skuStockService.getList(pid, keyword);
+        return CommonResult.success(skuStockList);
+    }
+    @ApiOperation("批量更新库存信息")
+    @RequestMapping(value ="/update/{pid}",method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult update(@PathVariable Long pid,@RequestBody List<PmsSkuStock> skuStockList){
+        int count = skuStockService.update(pid,skuStockList);
+        if(count>0){
+            return CommonResult.success(count);
+        }else{
+            return CommonResult.failed();
+        }
+    }
 }
--- mall-admin/src/main/java/com/macro/mall/service/impl/PmsSkuStockServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/PmsSkuStockServiceImpl.java
@@ -13,9 +13,28 @@ import java.util.List;
 
 /**
  * 商品sku库存管理Service实现类
- * Created by macro on 2018/4/27.
+ * Created by qx on 2024/4/23.
  */
 @Service
-public class PmsSkuStockServiceImpl  {
+public class PmsSkuStockServiceImpl implements PmsSkuStockService {
+    @Autowired
+    private PmsSkuStockMapper skuStockMapper;
+    @Autowired
+    private PmsSkuStockDao skuStockDao;
 
+    @Override
+    public List<PmsSkuStock> getList(Long pid, String keyword) {
+        PmsSkuStockExample example = new PmsSkuStockExample();
+        PmsSkuStockExample.Criteria criteria = example.createCriteria().andProductIdEqualTo(pid);
+        if (!StringUtils.isEmpty(keyword)) {
+            criteria.andSkuCodeLike("%" + keyword + "%");
+        }
+        return skuStockMapper.selectByExample(example);
+    }
+
+    @Override
+    public int update(Long pid, List<PmsSkuStock> skuStockList) {
+        return skuStockDao.replaceList(skuStockList);
+    }
 }
+
