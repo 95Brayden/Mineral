d1bbb47a4374e9565e7ca1dcf3e1767ca1603481
Shuhan Huang <1584267110@qq.com>
2024-04-23 15:59:39 +0800

    Create 全局过滤器配置
    --task=1023208 --user=黄舒涵111213 全局过滤器配置 https://www.tapd.cn/40489619/s/1088485

--- mall-gateway/src/main/java/com/macro/mall/filter/AuthGlobalFilter.java
+++ mall-gateway/src/main/java/com/macro/mall/filter/AuthGlobalFilter.java
@@ -17,10 +17,35 @@ import java.text.ParseException;
 
 /**
  * 将登录用户的JWT转化成用户信息的全局过滤器
- * Created by macro on 2020/6/17.
+ * Created by hsh on 2024/4/23.
  */
 @Component
-public class AuthGlobalFilter  {
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(AuthGlobalFilter.class);
 
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getHeaders().getFirst(AuthConstant.JWT_TOKEN_HEADER);
+        if (StrUtil.isEmpty(token)) {
+            return chain.filter(exchange);
+        }
+        try {
+            //从token中解析用户信息并设置到Header中去
+            String realToken = token.replace(AuthConstant.JWT_TOKEN_PREFIX, "");
+            JWSObject jwsObject = JWSObject.parse(realToken);
+            String userStr = jwsObject.getPayload().toString();
+            LOGGER.info("AuthGlobalFilter.filter() user:{}",userStr);
+            ServerHttpRequest request = exchange.getRequest().mutate().header(AuthConstant.USER_TOKEN_HEADER, userStr).build();
+            exchange = exchange.mutate().request(request).build();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
