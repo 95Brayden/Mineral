650560151dbe883a30ee6d3cc0c95619bfacd2bf
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-04-23 16:42:16 +0800

    OSS相关操作接口
    --task=1023221 --user=钱旭 OSS相关操作接口 https://www.tapd.cn/40489619/s/1089107

--- mall-admin/src/main/java/com/macro/mall/dto/OssCallbackParam.java
+++ mall-admin/src/main/java/com/macro/mall/dto/OssCallbackParam.java
@@ -6,9 +6,15 @@ import lombok.EqualsAndHashCode;
 
 /**
  * oss上传成功后的回调参数
- * Created by macro on 2018/5/17.
+ * Created by qx on 2024/4/23.
  */
-
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class OssCallbackParam {
-
+    @ApiModelProperty("请求的回调地址")
+    private String callbackUrl;
+    @ApiModelProperty("回调是传入request中的参数")
+    private String callbackBody;
+    @ApiModelProperty("回调时传入参数的格式，比如表单提交形式")
+    private String callbackBodyType;
 }
