b4ef9b43f71067f634f80895348bc7dd6ace556d
MXiangHAO <1953686710@qq.com>
2024-04-23 17:08:49 +0800

    global exception& auth controller
    --task=1023231 --user=龚仕元 auth认证控制 https://www.tapd.cn/40489619/s/1089439

--- mall-auth/src/main/java/com/macro/mall/auth/exception/Oauth2ExceptionHandler.java
+++ mall-auth/src/main/java/com/macro/mall/auth/exception/Oauth2ExceptionHandler.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 全局处理Oauth2抛出的异常
- * Created by macro on 2020/7/17.
+ * Created by mxh on 2024/4/23.
  */
 
+@ControllerAdvice
 public class Oauth2ExceptionHandler {
-
+    /**
+     * @ControllerAdvice 是 Spring MVC 提供的一个注解，用于全局处理控制器
+     */
+    @ResponseBody
+    @ExceptionHandler(value = OAuth2Exception.class)
+    public CommonResult handleOauth2(OAuth2Exception e) {
+        return CommonResult.failed(e.getMessage());
+    }
 }
