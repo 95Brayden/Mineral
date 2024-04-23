bd75cd15056fd55c49cb463d63818bb0b3e02896
Shuhan Huang <1584267110@qq.com>
2024-04-23 15:18:21 +0800

    Create
    --task=1023192 --user=黄舒涵111213 未登录/token过期异常信息返回 https://www.tapd.cn/40489619/s/1087681

--- mall-gateway/src/main/java/com/macro/mall/component/RestAuthenticationEntryPoint.java
+++ mall-gateway/src/main/java/com/macro/mall/component/RestAuthenticationEntryPoint.java
@@ -17,9 +17,19 @@ import java.nio.charset.Charset;
 
 /**
  * 自定义未登录或者token失效时的返回结果
- * Created by macro on 2020/6/18.
+ * Created by hsh on 2024/4/23.
  */
 @Component
-public class RestAuthenticationEntryPoint  {
-
+public class RestAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
+    @Override
+    public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException e) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.OK);
+        response.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        response.getHeaders().set("Access-Control-Allow-Origin","*");
+        response.getHeaders().set("Cache-Control","no-cache");
+        String body= JSONUtil.toJsonStr(CommonResult.unauthorized(e.getMessage()));//未登录、token过期返回结果异常信息
+        DataBuffer buffer =  response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
+        return response.writeWith(Mono.just(buffer));
+    }
 }
