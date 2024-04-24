661db8d99912f2fd5fd717cf2c84115b773b57f5
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-04-24 16:36:39 +0800

    首页新品管理
    --task=1023246 --user=钱旭 首页新品管理 https://www.tapd.cn/40489619/s/1089788

--- mall-admin/src/main/java/com/macro/mall/service/impl/SmsHomeNewProductServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/SmsHomeNewProductServiceImpl.java
@@ -13,9 +13,59 @@ import java.util.List;
 
 /**
  * 首页新品推荐管理Service实现类
- * Created by macro on 2018/11/6.
+ * Created by qx on 2024/4/24.
  */
 @Service
-public class SmsHomeNewProductServiceImpl  {
+public class SmsHomeNewProductServiceImpl implements SmsHomeNewProductService {
+    @Autowired
+    private SmsHomeNewProductMapper homeNewProductMapper;
+    @Override
+    public int create(List<SmsHomeNewProduct> homeNewProductList) {
+        for (SmsHomeNewProduct SmsHomeNewProduct : homeNewProductList) {
+            SmsHomeNewProduct.setRecommendStatus(1);
+            SmsHomeNewProduct.setSort(0);
+            homeNewProductMapper.insert(SmsHomeNewProduct);
+        }
+        return homeNewProductList.size();
+    }
 
+    @Override
+    public int updateSort(Long id, Integer sort) {
+        SmsHomeNewProduct homeNewProduct = new SmsHomeNewProduct();
+        homeNewProduct.setId(id);
+        homeNewProduct.setSort(sort);
+        return homeNewProductMapper.updateByPrimaryKeySelective(homeNewProduct);
+    }
+
+    @Override
+    public int delete(List<Long> ids) {
+        SmsHomeNewProductExample example = new SmsHomeNewProductExample();
+        example.createCriteria().andIdIn(ids);
+        return homeNewProductMapper.deleteByExample(example);
+    }
+
+    @Override
+    public int updateRecommendStatus(List<Long> ids, Integer recommendStatus) {
+        SmsHomeNewProductExample example = new SmsHomeNewProductExample();
+        example.createCriteria().andIdIn(ids);
+        SmsHomeNewProduct record = new SmsHomeNewProduct();
+        record.setRecommendStatus(recommendStatus);
+        return homeNewProductMapper.updateByExampleSelective(record,example);
+    }
+
+    @Override
+    public List<SmsHomeNewProduct> list(String productName, Integer recommendStatus, Integer pageSize, Integer pageNum) {
+        PageHelper.startPage(pageNum,pageSize);
+        SmsHomeNewProductExample example = new SmsHomeNewProductExample();
+        SmsHomeNewProductExample.Criteria criteria = example.createCriteria();
+        if(!StringUtils.isEmpty(productName)){
+            criteria.andProductNameLike("%"+productName+"%");
+        }
+        if(recommendStatus!=null){
+            criteria.andRecommendStatusEqualTo(recommendStatus);
+        }
+        example.setOrderByClause("sort desc");
+        return homeNewProductMapper.selectByExample(example);
+    }
 }
+
