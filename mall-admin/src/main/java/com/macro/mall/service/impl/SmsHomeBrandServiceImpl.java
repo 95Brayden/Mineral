220d75773d3c33d4d0d51d46b587817729afae68
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-04-24 16:31:19 +0800

    首页品牌管理
    --task=1023245 --user=钱旭 首页品牌管理 https://www.tapd.cn/40489619/s/1089785

--- mall-admin/src/main/java/com/macro/mall/service/impl/SmsHomeBrandServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/SmsHomeBrandServiceImpl.java
@@ -13,9 +13,59 @@ import java.util.List;
 
 /**
  * 首页品牌管理Service实现类
- * Created by macro on 2018/11/6.
+ * Created by qx on 2024/4/24.
  */
 @Service
-public class SmsHomeBrandServiceImpl  {
+public class SmsHomeBrandServiceImpl implements SmsHomeBrandService {
+    @Autowired
+    private SmsHomeBrandMapper homeBrandMapper;
+    @Override
+    public int create(List<SmsHomeBrand> homeBrandList) {
+        for (SmsHomeBrand smsHomeBrand : homeBrandList) {
+            smsHomeBrand.setRecommendStatus(1);
+            smsHomeBrand.setSort(0);
+            homeBrandMapper.insert(smsHomeBrand);
+        }
+        return homeBrandList.size();
+    }
 
+    @Override
+    public int updateSort(Long id, Integer sort) {
+        SmsHomeBrand homeBrand = new SmsHomeBrand();
+        homeBrand.setId(id);
+        homeBrand.setSort(sort);
+        return homeBrandMapper.updateByPrimaryKeySelective(homeBrand);
+    }
+
+    @Override
+    public int delete(List<Long> ids) {
+        SmsHomeBrandExample example = new SmsHomeBrandExample();
+        example.createCriteria().andIdIn(ids);
+        return homeBrandMapper.deleteByExample(example);
+    }
+
+    @Override
+    public int updateRecommendStatus(List<Long> ids, Integer recommendStatus) {
+        SmsHomeBrandExample example = new SmsHomeBrandExample();
+        example.createCriteria().andIdIn(ids);
+        SmsHomeBrand record = new SmsHomeBrand();
+        record.setRecommendStatus(recommendStatus);
+        return homeBrandMapper.updateByExampleSelective(record,example);
+    }
+
+    @Override
+    public List<SmsHomeBrand> list(String brandName, Integer recommendStatus, Integer pageSize, Integer pageNum) {
+        PageHelper.startPage(pageNum,pageSize);
+        SmsHomeBrandExample example = new SmsHomeBrandExample();
+        SmsHomeBrandExample.Criteria criteria = example.createCriteria();
+        if(!StringUtils.isEmpty(brandName)){
+            criteria.andBrandNameLike("%"+brandName+"%");
+        }
+        if(recommendStatus!=null){
+            criteria.andRecommendStatusEqualTo(recommendStatus);
+        }
+        example.setOrderByClause("sort desc");
+        return homeBrandMapper.selectByExample(example);
+    }
 }
+
