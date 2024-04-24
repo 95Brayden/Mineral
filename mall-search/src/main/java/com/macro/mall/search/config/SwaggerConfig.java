2cd00a3b895cbe47058409e790e91526fa074cf7
95Brayden <1875567407@qq.com>
2024-04-24 21:19:19 +0800

    change

--- mall-search/src/main/java/com/macro/mall/search/config/SwaggerConfig.java
+++ mall-search/src/main/java/com/macro/mall/search/config/SwaggerConfig.java
@@ -15,5 +15,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig extends BaseSwaggerConfig {
 
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.macro.mall.search.controller")
+                .title("mall搜索系统")
+                .description("mall搜索相关接口文档")
+                .contactName("macro")
+                .version("1.0")
+                .enableSecurity(false)
+                .build();
+    }
 
+    @Bean
+    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return generateBeanPostProcessor();
+    }
 }
