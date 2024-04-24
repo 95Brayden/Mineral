657cb36fa144fb75f5d6d79665960b554527fa77
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-04-24 15:51:44 +0800

    限时购活动管理
    --task=1023241 --user=钱旭 限时购活动管理 https://www.tapd.cn/40489619/s/1089773

--- mall-admin/src/main/java/com/macro/mall/service/impl/SmsFlashPromotionServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/SmsFlashPromotionServiceImpl.java
@@ -14,9 +14,50 @@ import java.util.List;
 
 /**
  * 限时购活动管理Service实现类
- * Created by macro on 2018/11/16.
+ * Created by qx on 2024/4/24.
  */
 @Service
-public class SmsFlashPromotionServiceImpl  {
+public class SmsFlashPromotionServiceImpl implements SmsFlashPromotionService {
+    @Autowired
+    private SmsFlashPromotionMapper flashPromotionMapper;
 
+    @Override
+    public int create(SmsFlashPromotion flashPromotion) {
+        flashPromotion.setCreateTime(new Date());
+        return flashPromotionMapper.insert(flashPromotion);
+    }
+
+    @Override
+    public int update(Long id, SmsFlashPromotion flashPromotion) {
+        flashPromotion.setId(id);
+        return flashPromotionMapper.updateByPrimaryKey(flashPromotion);
+    }
+
+    @Override
+    public int delete(Long id) {
+        return flashPromotionMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int updateStatus(Long id, Integer status) {
+        SmsFlashPromotion flashPromotion = new SmsFlashPromotion();
+        flashPromotion.setId(id);
+        flashPromotion.setStatus(status);
+        return flashPromotionMapper.updateByPrimaryKeySelective(flashPromotion);
+    }
+
+    @Override
+    public SmsFlashPromotion getItem(Long id) {
+        return flashPromotionMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List<SmsFlashPromotion> list(String keyword, Integer pageSize, Integer pageNum) {
+        PageHelper.startPage(pageNum, pageSize);
+        SmsFlashPromotionExample example = new SmsFlashPromotionExample();
+        if (!StringUtils.isEmpty(keyword)) {
+            example.createCriteria().andTitleLike("%" + keyword + "%");
+        }
+        return flashPromotionMapper.selectByExample(example);
+    }
 }
