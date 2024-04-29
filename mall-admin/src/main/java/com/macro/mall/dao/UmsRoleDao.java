a302e41dd36d03b626a8c16d0f15890b76a9f1f1
Shuhan Huang <1584267110@qq.com>
2024-04-29 16:31:14 +0800

    Create 后台角色管理
    --task=1023582 --user=黄舒涵111213 后台管理系统角色管理 https://www.tapd.cn/40489619/s/1098031

--- mall-admin/src/main/java/com/macro/mall/dao/UmsRoleDao.java
+++ mall-admin/src/main/java/com/macro/mall/dao/UmsRoleDao.java
@@ -8,8 +8,19 @@ import java.util.List;
 
 /**
  * 自定义后台角色管理Dao
- * Created by macro on 2020/2/2.
+ * Created by hsh on 2024/4/29.
  */
 public interface UmsRoleDao {
-
+    /**
+     * 根据后台用户ID获取菜单
+     */
+    List<UmsMenu> getMenuList(@Param("adminId") Long adminId);
+    /**
+     * 根据角色ID获取菜单
+     */
+    List<UmsMenu> getMenuListByRoleId(@Param("roleId") Long roleId);
+    /**
+     * 根据角色ID获取资源
+     */
+    List<UmsResource> getResourceListByRoleId(@Param("roleId") Long roleId);
 }
