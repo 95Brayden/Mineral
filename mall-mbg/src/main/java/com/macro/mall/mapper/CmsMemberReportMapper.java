698ffd79b8b566bd18d5fc69889320ee4c5f0b05
bdwang999 <148879578+bdwang999@users.noreply.github.com>
2024-05-06 15:37:29 +0800

    mapper层代码补充

--- mall-mbg/src/main/java/com/macro/mall/mapper/CmsMemberReportMapper.java
+++ mall-mbg/src/main/java/com/macro/mall/mapper/CmsMemberReportMapper.java
@@ -6,5 +6,17 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface CmsMemberReportMapper {
+    long countByExample(CmsMemberReportExample example);
 
+    int deleteByExample(CmsMemberReportExample example);
+
+    int insert(CmsMemberReport record);
+
+    int insertSelective(CmsMemberReport record);
+
+    List<CmsMemberReport> selectByExample(CmsMemberReportExample example);
+
+    int updateByExampleSelective(@Param("record") CmsMemberReport record, @Param("example") CmsMemberReportExample example);
+
+    int updateByExample(@Param("record") CmsMemberReport record, @Param("example") CmsMemberReportExample example);
 }
--- mall-mbg/src/main/java/com/macro/mall/mapper/CmsSubjectCommentMapper.java
+++ mall-mbg/src/main/java/com/macro/mall/mapper/CmsSubjectCommentMapper.java
@@ -6,5 +6,25 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface CmsSubjectCommentMapper {
+    long countByExample(CmsSubjectCommentExample example);
 
+    int deleteByExample(CmsSubjectCommentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CmsSubjectComment record);
+
+    int insertSelective(CmsSubjectComment record);
+
+    List<CmsSubjectComment> selectByExample(CmsSubjectCommentExample example);
+
+    CmsSubjectComment selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") CmsSubjectComment record, @Param("example") CmsSubjectCommentExample example);
+
+    int updateByExample(@Param("record") CmsSubjectComment record, @Param("example") CmsSubjectCommentExample example);
+
+    int updateByPrimaryKeySelective(CmsSubjectComment record);
+
+    int updateByPrimaryKey(CmsSubjectComment record);
 }
