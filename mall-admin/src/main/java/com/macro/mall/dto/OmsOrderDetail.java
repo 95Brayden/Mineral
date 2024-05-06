446551950e06e81861c3aa1604c7b4c1537e1ef7
Shuhan Huang <1584267110@qq.com>
2024-05-06 18:54:00 +0800

    Create dao
    1

--- mall-admin/src/main/java/com/macro/mall/dto/OmsOrderDetail.java
+++ mall-admin/src/main/java/com/macro/mall/dto/OmsOrderDetail.java
@@ -11,8 +11,15 @@ import java.util.List;
 
 /**
  * 订单详情信息
- * Created by macro on 2018/10/11.
+ * Created by hsh on 2024/5/6.
  */
 public class OmsOrderDetail extends OmsOrder {
-
+    @Getter
+    @Setter
+    @ApiModelProperty("订单商品列表")
+    private List<OmsOrderItem> orderItemList;
+    @Getter
+    @Setter
+    @ApiModelProperty("订单操作记录列表")
+    private List<OmsOrderOperateHistory> historyList;
 }
